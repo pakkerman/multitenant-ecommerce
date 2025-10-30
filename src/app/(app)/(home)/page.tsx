1d1d46cbfb140f35ae045ba108b36e49ca49b04c
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-30 14:53:59 +0800

    feat: payload integration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,29 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col gap-4 p-12 mt-32">
-      <div className="">
-        <Button variant="elevated">I'm a button</Button>
-      </div>
-      <div className="">
-        <Input placeholder="I'm an input" />
-      </div>
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
 
-      <div className="">
-        <Progress value={50} />
-      </div>
+  const data = await payload.find({
+    collection: "users",
+  });
 
-      <div className="">
-        <Textarea placeholder="I'm a textarea" />
-      </div>
-      <div className="">
-        <Checkbox />
-      </div>
-    </div>
-  );
+  return <div className="">{JSON.stringify(data, null, 2)}</div>;
 }

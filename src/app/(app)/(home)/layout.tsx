a54af1c5f34b9c2ba683e12ecded870dcf53778b
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-31 15:58:53 +0800

    feat: dropdowns layout

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -26,6 +26,7 @@ const Layout = async ({ children }: Props) => {
         exists: false,
       },
     },
+    sort: "name",
   });
 
   const formattedData: CustomCategory[] = data.docs.map((doc) => ({

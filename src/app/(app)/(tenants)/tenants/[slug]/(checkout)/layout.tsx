401fbbce4007b560def19d8ffe80f41cdfe75df9
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-11-13 16:18:05 +0800

    feat: checkout flow

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(checkout)/layout.tsx
@@ -0,0 +1,23 @@
+import { Navbar } from "@/modules/checkout/ui/components/navbar";
+import { Footer } from "@/modules/tenants/ui/components/footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: Promise<{ slug: string }>;
+}
+
+const Layout = async ({ children, params }: LayoutProps) => {
+  const { slug } = await params;
+
+  return (
+    <div className="flex min-h-screen flex-col bg-[#f4f4f0]">
+      <Navbar slug={slug} />
+      <div className="flex-1">
+        <div className="mx-auto max-w-(--breakpoint-xl)">{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "../init";
 
 import { authRouter } from "@/modules/auth/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { checkoutRouter } from "@/modules/checkout/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedure";
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   products: productsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
 });
 
 // export type definition of API

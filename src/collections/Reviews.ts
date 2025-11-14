7f76a6f64deefafb21bc5d5aec913b07e79053fe
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-11-14 17:34:14 +0800

    feat: add reviews collection

---
+++ src/collections/Reviews.ts
@@ -0,0 +1,36 @@
+import type { CollectionConfig } from "payload";
+
+export const Reviews: CollectionConfig = {
+  slug: "reviews",
+  admin: {
+    useAsTitle: "description",
+  },
+  fields: [
+    {
+      name: "description",
+      type: "textarea",
+      required: true,
+    },
+    {
+      name: "rating",
+      type: "number",
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    {
+      name: "product",
+      type: "relationship",
+      relationTo: "products",
+      required: true,
+      hasMany: false,
+    },
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+    },
+  ],
+};

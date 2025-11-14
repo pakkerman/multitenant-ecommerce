44359d6561af345957255bdc79ae896b7568371e
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-11-14 16:31:54 +0800

    feat: library page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,6 +3,7 @@ import { createTRPCRouter } from "../init";
 import { authRouter } from "@/modules/auth/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { checkoutRouter } from "@/modules/checkout/server/procedures";
+import { libraryRouter } from "@/modules/library/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedure";
@@ -14,6 +15,7 @@ export const appRouter = createTRPCRouter({
   products: productsRouter,
   tenants: tenantsRouter,
   checkout: checkoutRouter,
+  library: libraryRouter,
 });
 
 // export type definition of API

f64c8aeb8f70612640856a29713927dae4c68670
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-11-15 14:08:52 +0800

    feat: add review data to products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -155,9 +155,33 @@ export const productsRouter = createTRPCRouter({
         limit: input.limit,
       });
 
+      const dataWithSummarizedReview = await Promise.all(
+        data.docs.map(async (doc) => {
+          const reviewsData = await ctx.db.find({
+            collection: "reviews",
+            pagination: false,
+            where: {
+              product: {
+                equals: doc.id,
+              },
+            },
+          });
+
+          return {
+            ...doc,
+            reviewCount: reviewsData.totalDocs,
+            reviewRating:
+              reviewsData.docs.length === 0
+                ? 0
+                : reviewsData.docs.reduce((acc, curr) => acc + curr.rating, 0) /
+                  reviewsData.totalDocs,
+          };
+        }),
+      );
+
       return {
         ...data,
-        docs: data.docs.map((doc) => ({
+        docs: dataWithSummarizedReview.map((doc) => ({
           ...doc,
           image: doc.image as Media | null,
           tenant: doc.tenant as Tenant & { image: Media | null },
